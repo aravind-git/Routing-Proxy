deb04fe7bfb12352a3d8e48e8fe062060d3e71c5
Aravind Sridhar <itzaravindsridhar@gmail.com>
2024-04-20 22:12:51 +0530

    Adding JSON Mock

--- router/src/main/java/com/example/router/controller/ProxyController.java
+++ router/src/main/java/com/example/router/controller/ProxyController.java
@@ -2,18 +2,24 @@ package com.example.router.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+
+import org.springframework.web.bind.annotation.PathVariable;
+
 @RestController
 public class ProxyController {
+
 
     @Autowired
     RestTemplate restTemplate;
 
-    @GetMapping(path="/route")
-    public String routeToProxy(){
+    @GetMapping(path="/route/{url}",produces= MediaType.APPLICATION_JSON_VALUE)
+    public String routeToProxy(@PathVariable String url){
 
         String destinationUrl = "https://catfact.ninja/fact";
 
